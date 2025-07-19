e06b46746377566f90d670ba76434cc1e34ee529
Imanuel Pundoko <ilomon10@gmail.com>
2025-07-19 08:58:23 +0800

    feat: add gps function

--- src/http-server.ts
+++ src/http-server.ts
@@ -1,10 +1,15 @@
 import { Application, Router } from "oak";
+import GPS from "./lib/gps/gps.ts";
 
 const http_server = new Application();
 
 const router = new Router();
 
-router.get("api/health", (ctx) => {
+router.get("/", (ctx) => {
+  ctx.response.body = "hello world";
+});
+
+router.get("/api/health", (ctx) => {
   ctx.response.body = {
     status: "OK",
     io: {
@@ -12,6 +17,26 @@ router.get("api/health", (ctx) => {
       can: "FAILED",
       gpio: "FAILED",
     },
+  };
+});
+
+const gps = new GPS({
+  auto: true,
+});
+
+router.get("/api/gps", async (ctx) => {
+  const list_usb = await gps.list();
+  console.log(list_usb);
+  ctx.response.body = {
+    list_usb,
+  };
+});
+
+router.get("/api/gps/list", async (ctx) => {
+  const list_usb = await gps.list();
+  console.log(list_usb);
+  ctx.response.body = {
+    list_usb,
   };
 });
 
--- src/lib/gps/gps.ts
+++ src/lib/gps/gps.ts
@@ -1,3 +1,4 @@
+import { ReadlineParser, SerialPort } from "serialport";
 import { createNanoEvents, Emitter } from "nanoevents";
 
 type GPSOptions = {
@@ -11,9 +12,29 @@ interface GPSEvents {
 class GPS {
   private options: GPSOptions;
   private emitter: Emitter<GPSEvents>;
+  private port: SerialPort;
   constructor(options: GPSOptions) {
     this.emitter = createNanoEvents<GPSEvents>();
     this.options = options;
+    this.port = new SerialPort({
+      path: "COM11",
+      baudRate: 115200,
+      autoOpen: true,
+    });
+
+    this.setup();
+  }
+
+  setup() {
+    // const parser = new ReadlineParser();
+    // this.port.pipe(parser);
+    // this.port.on("data", (data) => {
+    //   console.log("DATA: ", data);
+    // });
+  }
+
+  async list() {
+    return await SerialPort.list();
   }
 
   on<E extends keyof GPSEvents>(event: E, callback: GPSEvents[E]) {
@@ -29,3 +50,25 @@ class GPS {
 }
 
 export default GPS;
+
+if (import.meta.main) {
+  const port = new SerialPort({
+    path: "COM11",
+    baudRate: 115200,
+    autoOpen: true,
+  });
+  console.log("RUNNING");
+
+  port.on("open", () => {
+    console.log("OPEN");
+  });
+  port.on("data", (data) => {
+    console.log("DATA:", data);
+  });
+  port.on("error", (err) => {
+    console.error(err);
+  });
+  port.on("close", () => {
+    console.log("CLOSE");
+  });
+}
